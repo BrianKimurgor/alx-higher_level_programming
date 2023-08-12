500af7b02b37189941d871915f01dd1a9b774564
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-08-12 06:04:51 -0700

    column correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for col in row:
-            print("{:d}".format(col), end=" " if col != row[-1] else "")
-            print()
+        for column in row:
+            print("{:d}".format(column), end="")
+            if column != row[-1]:
+                print(" ", end="")
+        print()
