6a7589f3c1a92345b361f9a7961ae0e31f1eb0ba
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-08-28 08:15:29 -0700

    advanced correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,15 +2,16 @@
 
 import sys
 
+
 def safe_print_integer_err(value):
     """
     Prints an integer.
-    
+
     Args:
         value: The value to print.
-    
+
     Returns:
-        True if value has been correctly printed (it means the value is an integer),
+        True if value correctly printed ( meaning the value is an integer),
         False otherwise.
     """
     try:
