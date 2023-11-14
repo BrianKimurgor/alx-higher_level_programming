dd0e68dc3cecabb83a23f20a927b4cf83d8a50ed
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-11-14 17:50:30 +0300

    my time test

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,9 +5,9 @@ class Rectangle {
     if ((w > 0 && h > 0)) {
       this.width = w;
       this.height = h;
-    } else {
+    } /*else {
       return {};
-    }
+    }*/
   }
 }
 module.exports = Rectangle;
