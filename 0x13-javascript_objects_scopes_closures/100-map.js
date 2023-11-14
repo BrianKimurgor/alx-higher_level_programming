5c39f269427d938c86b67121032559b6a227d118
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-11-14 23:08:21 +0300

    commits final

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const { list } = require('./100-data');
+
+const mappedList = list.map((value, index) => value * index);
+
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(mappedList);
