8d748c0561d0ed9f49d515eab705fd0864c0678d
BrianKimurgor <kimurgorbrian20@gmail.com>
2023-11-19 10:31:22 +0300

    mine

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""#!/usr/bin/python3
+""lists all states from the database hbtn_0e_0_usa""
 import MySQLdb
 import sys
 
@@ -19,3 +19,34 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
+"""
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(256), nullable=False)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('N%')).order_by(State.id).all()
+
+    for state in states:
+        print(state.id, state.name)
+
+    session.close()
