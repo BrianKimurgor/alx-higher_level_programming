f09458319b6565110d048d9fbd08c7d2328b010c
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-03 19:15:52 +0300

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
-//script that reads and prints contents of a file
+// script that reads and prints contents of a file
 
-const fs = require('fs')
+const fs = require('fs');
 
-//getting the filepath from command line arguments
-const filepath = process.argv[2]
+// getting the filepath from command line arguments
+const filepath = process.argv[2];
 
-//funtion to read the file
-function readFile(path) {
-    fs.readFile(path, 'utf8', (err, data) => {
-        if(err){
-            console.error(err);
-        }else{
-            console.log(data);
-        }
-    });
+// funtion to read the file
+function readFile (path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+    }
+  });
 }
-//calling the function with the filepath
+// calling the function with the filepath
 readFile(filepath);
