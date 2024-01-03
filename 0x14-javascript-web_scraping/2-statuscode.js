5439dd90e64b08da18cb4b9281997637d38731b9
BrianKimurgor <kimurgorbrian20@gmail.com>
2024-01-03 20:31:39 +0300

    request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,22 +8,22 @@
 
 const request = require('request');
 
-// getting URL from the command line argumenst
+// getting URL from the command line arguments
 const url = process.argv[2];
 
 // function to send Get request
-function getRequest (url) {
+function getRequest(url) {
   request({
     url,
     method: 'GET'
-  }, (error, responce, body) => {
+  }, (error, response, body) => {
     if (error) {
       console.error(`Error ${error}`);
     } else {
-      console.log('code:{$response.statusCode}');
+      console.log(`code:${response.statusCode}`); // Corrected string interpolation here
     }
   });
 }
 
-// call the function within the URL
+// call the function with the URL
 getRequest(url);
